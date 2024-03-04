39e19f5594d446b0cc5fb71f4fc9cd1a007ae56d
Ernesto Torralba <e.torralba.ba@gmail.com>
2024-03-04 15:05:22 -0600

    ✨ feat: add route for home

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Note the change here
+import Home from './pages/Home';
 
-function App() {
-  return (
-      <h1 className="text-3xl font-bold underline text-red-600">
-        Simple React Typescript Tailwind Sample
-      </h1>
-  );
+const App: React.FC = () => {
+    return (
+        <Router>
+            <div>
+                {/* Common components like header and footer can be included here */}
+                <Routes> {/* Use Routes instead of Switch */}
+                    <Route path="/" element={<Home />} /> {/* Use "element" prop instead of "component" */}
+                </Routes>
+                {/* Common components like footer can be included here */}
+            </div>
+        </Router>
+    );
 }
 
 export default App;
